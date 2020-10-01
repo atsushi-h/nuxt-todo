817d59eadb8195644600746fe3f6f07b4ea34394
atsushi-h <atsushi.1213eng@gmail.com>
2020-10-01 09:04:17 +0900

    Logoutボタンを追加

---
+++ todo-app/src/components/LogoutButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-btn v-if="isLoggedIn" @click="logout" elevation="2">logout</v-btn>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { UserModule } from '../store';
+
+export default Vue.extend({
+  name: 'LogoutButton',
+  methods: {
+    logout() {
+      UserModule.logout();
+    },
+  },
+  computed: {
+    isLoggedIn() {
+      return UserModule.isLoggedIn;
+    },
+  },
+});
+</script>
